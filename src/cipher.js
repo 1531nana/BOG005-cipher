f28d74ce9353cbfca5f3500a7f86dee3aa758ad8
Adriana Giraldo <1531nana@gmail.com>
2022-07-14 12:57:24 -0500

    mejoras en la lsl llamadas a las ciones,agre gué contenido al css y a djuntÃéos imágenes en el src

--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,79 @@
-// const cipher = {
-//   // ...
-// };
-
-// export default cipher;
 const cipher = {
+  //función para codificar el mensaje
   encode: function (offset, mensajeACifrar){
     let resul = "";
+    // console.log(offset, mensajeACifrar);
+    //condicionales para validar la información necesaria
+    if(offset.value !== Number){
+      throw TypeError("debe ingresar un número");
+    }
+    if(offset.value == ""){
+      throw TypeError(alert("Debe ingresar un número"));
+    }
+    if(offset.value == 0 ){
+      throw TypeError(alert("Debe ingresar un número mayor a cero"));
+    }  else if(mensajeACifrar == ""){
+      throw TypeError(alert(alert("Debe ingresar un mensaje")));
+    } else  if(offset.value < 0){
+      throw TypeError(alert("Debe ingresar un número positivo"));
+    }  else {
     if(offset){
       //retorna la cadena de caracteres en un constructor
-      mensajeACifrar = new String(cifrar.value.toUpperCase());
+      mensajeACifrar = new String(mensajeACifrar.toUpperCase());
       console.log(mensajeACifrar);
-      //bucle para recorrer toda la cadena de caracteres (mensaje a cifrar)
+      //bucle para recorrer toda la cadena de caracteres (mensaje a mensajeACifrar)
       for(let i = 0; i < mensajeACifrar.length; i++){
         //método para que muestre el código ASCII de la letra
         let resultado = mensajeACifrar.charCodeAt([i]);
         console.log(resultado);
-        // variable que muestra en código ASCII, el nuevo código con el desplazamiento que el usuario indicó
-        let formula = (parseInt(resultado)- 65 + parseInt(desplazamiento.value))%26+65;
+        // variable que muestra en código ASCII, el nuevo código con el offset que el usuario indicó
+        let formula = (parseInt(resultado)- 65 + parseInt(offset.value))%26+65;
         console.log(formula);
         //muestra la letra respecto al código ASCII
         let mensajeYaCifrado = String.fromCharCode([formula]);
         resul += mensajeYaCifrado;
       }
-      alert("su mensaje cifrado es: "+resul);
       return(resul);
     }
+  }
   },
 
-
+//función para decodificar el mensaje 
   decode: function(offset, mensajeADescifrar){
     let resul1 = "";
-    if(offset){
-      mensajeADescifrar = new String(descifrar.value.toUpperCase());
+    // condicionales para validar la información necesaria
+    if(offset.value !== Number){
+      throw TypeError("debe ingresar un número");
+    }
+    if(offset.value == ""){
+      throw TypeError(alert("Debe ingresar un número"));
+    }
+     if(offset.value == 0){
+      throw TypeError(alert("Debe ingresar un número mayor a cero"));
+    } else if(mensajeADescifrar == ""){
+      throw TypeError(alert("Debe ingresar un mensaje"));
+    } else  if(offset.value < 0){
+      throw TypeError(alert("Debe ingresar un número positivo"));
+    } else { 
+      if(offset){
+        // variable que va a pasar a un constructor el mensaje
+      mensajeADescifrar = new String(mensajeADescifrar.toUpperCase());
       console.log(mensajeADescifrar);
+      // ciclo para iterar por cada caracter del mensaje
       for(let i = 0; i < mensajeADescifrar.length; i++){
+        //variable que muestra la posición del caracter en el código ASCII
         let resultadoDescifrar = mensajeADescifrar.charCodeAt([i]);
         console.log(resultadoDescifrar);
-        let resta = resultadoDescifrar - desplazamiento.value;
+        //variable que permite mostrar el código ASCII descifrado
+        let resta = resultadoDescifrar - offset.value;
         console.log(resta);
+        // variable que permite mostrar la letra descifrada, respecto al código ASCII
         let codigoDescifrado = String.fromCharCode([resta]);
         resul1 += codigoDescifrado;
       }
-      document.write("su código descifrado es: "+resul1);
+      return(resul1);
     }
+  }
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-// import cipher from './cipher.js';
-
-// console.log(cipher);
-
 import cipher from './cipher.js';
-let offset = document.getElementById("desplazamiento");
+// let offset = document.getElementById("desplazamiento");
 let btnMensajeACifrar = document.getElementById("enviar_cifrado");
-let mensajeACifrar = document.getElementById("cifrar").value;
+// let mensajeACifrar = document.getElementById("cifrar").value;
 let btnMensajeADescifrar = document.getElementById("enviar_descifrado");
-let mensajeADescifrar = document.getElementById("descifrar").value;
+// let mensajeADescifrar = document.getElementById("descifrar").value;
 
 
 //botón que muestra el mensaje cifrado al ser llamado
-btnMensajeACifrar.addEventListener("click", cipher.encode);
+btnMensajeACifrar.addEventListener("click", cifrar);
+function cifrar(){
+    let mensajeACifrar = document.getElementById("cifrar").value;
+    let offset = document.getElementById("desplazamiento");
+    let resultadoCifrar = cipher.encode(offset, mensajeACifrar);
+    console.log(cipher.encode(offset, mensajeACifrar));
+   if(offset && mensajeACifrar){
+       document.write("Su mensaje cifrado es: "+resultadoCifrar);
+   } 
 
+}
 //botón para descifrar el mensaje
-btnMensajeADescifrar.addEventListener("click", cipher.decode);
-
-
-
-
-
-
-// console.log(cipher.encode());
+btnMensajeADescifrar.addEventListener("click", descifrar);
+function descifrar (){
+let offset = document.getElementById("desplazamiento");
+let mensajeADescifrar = document.getElementById("descifrar").value;
+let resultadoDescifrar = cipher.decode(offset, mensajeADescifrar);
+if(offset && mensajeADescifrar){
+    document.write("su mensaje descifrado es: " + resultadoDescifrar);
+}
+}
