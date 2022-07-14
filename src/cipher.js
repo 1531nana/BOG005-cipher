5e4a83b8fcbfadbf00098fd00940a63b824da621
Adriana Giraldo <1531nana@gmail.com>
2022-07-13 19:13:48 -0500

    funcionalidad a encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
+// const cipher = {
+//   // ...
+// };
+
+// export default cipher;
 const cipher = {
-  // ...
+  encode: function (offset, mensajeACifrar){
+    let resul = "";
+    if(offset){
+      //retorna la cadena de caracteres en un constructor
+      mensajeACifrar = new String(cifrar.value.toUpperCase());
+      console.log(mensajeACifrar);
+      //bucle para recorrer toda la cadena de caracteres (mensaje a cifrar)
+      for(let i = 0; i < mensajeACifrar.length; i++){
+        //método para que muestre el código ASCII de la letra
+        let resultado = mensajeACifrar.charCodeAt([i]);
+        console.log(resultado);
+        // variable que muestra en código ASCII, el nuevo código con el desplazamiento que el usuario indicó
+        let formula = (parseInt(resultado)- 65 + parseInt(desplazamiento.value))%26+65;
+        console.log(formula);
+        //muestra la letra respecto al código ASCII
+        let mensajeYaCifrado = String.fromCharCode([formula]);
+        resul += mensajeYaCifrado;
+      }
+      alert("su mensaje cifrado es: "+resul);
+      return(resul);
+    }
+  },
+
+
+  decode: function(offset, mensajeADescifrar){
+    
+  }
 };
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import cipher from './cipher.js';
+// import cipher from './cipher.js';
 
-console.log(cipher);
+// console.log(cipher);
+
+import cipher from './cipher.js';
+let offset = document.getElementById("desplazamiento");
+let btnMensajeACifrar = document.getElementById("enviar_cifrado");
+let mensajeACifrar = document.getElementById("cifrar").value;
+let btnMensajeADescifrar = document.getElementById("enviar_descifrado");
+let mensajeADescifrar = document.getElementById("descifrar").value;
+
+
+//botón que muestra el mensaje cifrado al ser llamado
+btnMensajeACifrar.addEventListener("click", cipher.encode);
+
+// cipher.encode(offset, mensajeACifrar);
+//boton para cifrar mensaje
+// cipher.encode(offset, mensajeACifrar);
+// btnMensajeACifrar.addEventListener("click", cipher.encode());
+
+//botón para descifrar el mensaje
+btnMensajeADescifrar.addEventListener("click", cipher.decode);
+
+
+
+
+
+
+// console.log(cipher.encode());
