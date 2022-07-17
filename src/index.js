9fe608039155208d4de23dbbb5a3a9373ea955aa
Adriana Giraldo <1531nana@gmail.com>
2022-07-16 19:40:30 -0500

    agregué input, para mostarle ahí el mensaje al usuario, agregando en HTML y CSS, modifiqué index.js

--- src/index.js
+++ src/index.js
@@ -42,28 +42,34 @@ let btnMensajeACifrar = document.getElementById("enviar_cifrado");
 // let mensajeACifrar = document.getElementById("cifrar").value;
 let btnMensajeADescifrar = document.getElementById("enviar_descifrado");
 // let mensajeADescifrar = document.getElementById("descifrar").value;
-
+// let resultadoUsuario = document.getElementById("caja_resultado");
+// let resultadoUsuario;
 
 //botón que muestra el mensaje cifrado al ser llamado
 btnMensajeACifrar.addEventListener("click", cifrar);
 function cifrar(){
     let mensajeACifrar = document.getElementById("cifrar").value;
-    let offset = document.getElementById("desplazamiento").value;   
-    // console.log(typeof offset);
+    let offset = document.getElementById("desplazamiento").value;
     let resultadoCifrar = cipher.encode(offset, mensajeACifrar);
     console.log(cipher.encode(offset, mensajeACifrar));
-   if(offset && mensajeACifrar){
-       document.write("Su mensaje cifrado es: " + resultadoCifrar);
-   } 
+    // function mostrarResultado (){
+    // document.getElementById("caja_resultado").value;
+        if(offset && mensajeACifrar){
+    document.getElementById("caja_resultado").value = "su mensaje cifrado es: "+resultadoCifrar;
+        //     resultadoUsuario.value +=  resultadoCifrar;
+        //  console.log("su mensaje cifrado es: "+resultadoUsuario);
+        } 
+    }
 
-}
+// }
 //botón para descifrar el mensaje
 btnMensajeADescifrar.addEventListener("click", descifrar);
 function descifrar (){
 let offset = document.getElementById("desplazamiento").value;
 let mensajeADescifrar = document.getElementById("descifrar").value;
+// let resultadoUsuario = document.getElementById("caja_resultado");
 let resultadoDescifrar = cipher.decode(offset, mensajeADescifrar);
 if(offset && mensajeADescifrar){
-    document.write("su mensaje descifrado es: " + resultadoDescifrar);
+    document.getElementById("caja_resultado").value = "su mensaje descifrado es: "+ resultadoDescifrar;
 }
 }
